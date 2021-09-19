fc92ecab566e2e4b1f15cce82022a283298f6b8d
Guido Cotelesso <guido.cotelesso@fpayapp.com>
2021-09-18 22:06:47 -0300

    [FIX]Arreglo de estilos y avance en el diseño de ilista de items

--- src/views/Items.tsx
+++ src/views/Items.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import * as queryString from "query-string";
 import { IGetItems } from '../types/getItems';
@@ -10,10 +9,9 @@ import { useCapitalize } from '../hooks/useCapitalize';
 export const Items = () => {
         
     const location = useLocation<IGetItems>();
-    const { q: item } =  queryString.parse(location.search);
+    const { q: title } =  queryString.parse(location.search);
     const { items } = location.state;
-    const capitalizedTitle = useCapitalize(item.toString());
-    console.log(items)
+    const capitalizedTitle = useCapitalize(title.toString());
 
     return (
         <>
@@ -27,26 +25,16 @@ export const Items = () => {
                     items.map(({picture, price, id, title, seller_address}) => {
                         return (
                             <div key={id} className="product-container">
-                                <div style={{
-                                    height: '200px',
-                                    width: '200px',
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                }}>
                                     <img src={picture} style={{
-                                        height: '200px',
-                                        width:'200px',
+                                        width: '160px',
+                                        height: '160px',
                                         objectFit: 'contain'
                                     }}/>
-                                </div>
-                                <div>
+                                <div className="title-container">
                                     <h3>{useCurrency({...price})}</h3>
                                     <h4>{title}</h4>
                                 </div>
-                                <div>
-                                    <h4>{seller_address}</h4>
-                                </div>
+                                <h4 style={{marginTop: '2em'}}>{seller_address}</h4>
                             </div>
                         )
                     })
