d87400a1b16c146fb6bc23dd57b4af967b6b511c
Guido Cotelesso <guido.cotelesso@fpayapp.com>
2021-09-20 00:23:43 -0300

    [ADD]Se continua sobre el diseño de detalle de items

--- src/views/ItemDetail.tsx
+++ src/views/ItemDetail.tsx
@@ -1,20 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, } from "react";
 import { useLocation } from "react-router-dom";
 import { apiGetItemDetails } from "../api/itemDetail";
-import { IGetItemDetail } from "../types/getItemDetail";
+import { IGetItemDetail, Item } from "../types/getItemDetail";
 import { Spinner } from "../components/loader/Spinner";
+import useCurrency from "../hooks/useCurrency";
 
 export const ItemDetail = () => {
 
     const location = useLocation<IGetItemDetail>();
-    console.log(location.pathname.slice(7, location.pathname.length));
     const [isLoading, setIsLoading] = useState(true);
-    let item: IGetItemDetail
+    const [item, setItem] = useState<Item>();
+    const [price, setPrice] = useState<string>('');
 
     useEffect(() => {
         apiGetItemDetails(location.pathname.slice(7, location.pathname.length)).then((res) => {
-            console.log(res.data);
-            item = res.data;
+            setItem(res.data.item);
+            console.log(res.data.item)
+            setPrice(() => useCurrency(res.data.item.price));
             setIsLoading(false)
         }).catch((error) => {
             setIsLoading(false);
@@ -25,8 +27,23 @@ export const ItemDetail = () => {
             (
                 isLoading
                 ? <Spinner />
-                : <div>
-
+                : <div className="item-detail-page">
+                    <div className="item-detail-container">
+                            <div className="item-image-price-container">
+                                <img src={item.picture} className="item-detail-image"/>
+                                <div className="item-detail-condition-selled">
+                                <h6>
+                                    {item.condition} - {item.sold_quantity} vendido(s)
+                                </h6>
+                                <h6 className="item-detail-title">
+                                    {item.title}
+                                </h6>
+                                <h6>
+                                    {price}
+                                </h6>
+                                </div>
+                            </div>
+                    </div>
                 </div>
             )
     )
