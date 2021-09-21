6ea42be4e08df195235f8744f5c960c73fab3bb5
Guido Cotelesso <guido.cotelesso@fpayapp.com>
2021-09-21 17:40:21 -0300

    [ADD]Seo para pagina not found

--- src/components/item-detail/ItemDetailContainer.tsx
+++ src/components/item-detail/ItemDetailContainer.tsx
@@ -1,12 +1,16 @@
+import { useCapitalize } from "../../hooks/useCapitalize";
 import { Item } from "../../types/getItemDetail"
 import { Seo } from "../seo/Seo";
 import { ImagePriceContainer } from "./ImagePriceContainer";
 import { ItemDetailDescription } from "./ItemDetailDescription";
 
 export const ItemDetailContainer = (props: Item) => {
+
+    const title = useCapitalize(props.title)
+
     return (
             <>
-                <Seo title={props.title}/>
+                <Seo title={title}/>
                 <div className="item-detail-container">
                     <ImagePriceContainer {...props} />
                     <ItemDetailDescription  description={props.description} />
--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,13 +1,17 @@
 import { Link } from 'react-router-dom'
 import NotFoundSvg from '../assets/images/NotFound.svg'
+import { Seo } from '../components/seo/Seo'
 
 export const NotFound = () => {
 
     return (
-        <div className="not-found-container">
-            <img src={NotFoundSvg} />
-            <h3>La pagina solicitada no existe</h3>
-            <Link to="/" className="not-found-link">Ir a la pagina principal</Link>
-        </div>
+        <>
+            <Seo title="Not Found" />
+            <div className="not-found-container">
+                <img src={NotFoundSvg} />
+                <h3>La pagina solicitada no existe</h3>
+                <Link to="/" className="not-found-link">Ir a la pagina principal</Link>
+            </div>
+        </>
     )
 }
